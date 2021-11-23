144a77d1ac33e5112f97f3e38a93a501ad5c052c
lyrarod <40926108+lyrarod@users.noreply.github.com>
2021-11-22 22:47:59 -0300

    commit via codesandbox

--- src/components/data.js
+++ src/components/data.js
@@ -1,5 +1,7 @@
 export const data = [
-  { id: 1, text: "ReactJS 😀", isCompleted: false },
-  { id: 2, text: "NextJS 😁", isCompleted: false },
-  { id: 3, text: "Javascript Vanilla 😅", isCompleted: true }
+  { id: 1, text: "Hello World! 😀", isCompleted: false },
+  { id: 2, text: "Olá Mundo! 👀", isCompleted: true },
+  { id: 3, text: "ReactJS 😎", isCompleted: false },
+  { id: 4, text: "NextJS 😁", isCompleted: false },
+  { id: 5, text: "Javascript Vanilla 🐱‍👤", isCompleted: false }
 ];
